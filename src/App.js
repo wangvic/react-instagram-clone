58ea65fabd3a795cc63e655ccf411f38cd912322
Lei Wang (Vic) <vic.leiwang@gmail.com>
2020-08-02 23:10:52 +0100

    Added modal for sign up

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [posts, setPosts] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = React.useState(getModalStyle);
 
   // UseEffect runs a piece of code based on a specific condition
   useEffect(() => {
@@ -12,16 +41,23 @@ function App() {
     db.collection('posts').onSnapshot((snapshot) => {
       // Every time a new post is added, this code fires
       setPosts(
-        snapshot.docs.map((doc) => {
-          doc.id;
-          doc.data();
-        })
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
       );
     });
   }, []);
 
+  // const signUp = (event) => {};
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -29,6 +65,8 @@ function App() {
           alt=""
         />
       </div>
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       {posts.map(({ id, post }) => (
         <Post
