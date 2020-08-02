3bf4ccd2fbaf1a927c4eb0ec8d9aa5ac7eb4916f
Lei Wang (Vic) <vic.leiwang@gmail.com>
2020-08-02 13:17:20 +0100

    add static state for posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: 'vic.leiwang',
+      caption: 'React JS is cool!',
+      imageUrl:
+        'https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png',
+    },
+    {
+      username: 'tanyayao',
+      caption: 'Flutter is better!',
+      imageUrl: 'https://hackr.io/blog/react-native-vs-flutter/thumbnail/large',
+    },
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,18 +27,13 @@ function App() {
         />
       </div>
 
-      <h1>Hello World 🚀!</h1>
-
-      <Post
-        username="vic.leiwang"
-        caption="React JS is cool!"
-        imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-      />
-      <Post
-        username="tanyayao"
-        caption="Flutter is better!"
-        imageUrl="https://hackr.io/blog/react-native-vs-flutter/thumbnail/large"
-      />
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
