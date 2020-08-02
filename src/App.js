dc81ffeccb4e07eba992b5a98c263987628b720f
Lei Wang (Vic) <vic.leiwang@gmail.com>
2020-08-02 11:57:15 +0100

    create header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World🚀</h1>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt=""
+        />
+      </div>
+
+      <h1>Hello World 🚀!</h1>
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
